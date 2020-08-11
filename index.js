c7e4f1592e45f1952d63c70484155ff3e0b2d1fd
muNeves3 <murilogrilosn@gmail.com>
2020-08-11 13:08:52 -0300

    :rocket: main code

---
+++ index.js
@@ -0,0 +1,35 @@
+const puppeteer = require("puppeteer");
+const fs = require("fs");
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto("https://instagram.com/rocketseat_oficial");
+
+  const imgList = await page.evaluate(() => {
+    //toda essa função será executada no browser
+
+    //vamos pegar todas as imagens que estão na parte de posts
+    const nodeList = document.querySelectorAll("article img");
+
+    //transformar o NodeList em array
+    const imgArray = [...nodeList];
+
+    //transformar os nodes(elementos html) em objetos javascript
+    const imgList = imgArray.map(({ src }) => ({ src }));
+
+    //colocar para fora da função
+    return imgList;
+  });
+
+  //escrever os dados em um arquivo local
+
+  fs.writeFile("instagram.json", JSON.stringify(imgList, null, 2), (err) => {
+    if (err) throw new Error("something went wrong");
+
+    console.log("Well done!");
+  });
+  await browser.close();
+})();
+
+//funnção autoexecutável, mais rápida do que fazer uma função e rodar dps
